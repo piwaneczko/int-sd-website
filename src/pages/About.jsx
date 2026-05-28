759cbd410591402c9fd29ab1f2ab37cb422abf61
Paweł Iwaneczko <p.iwaneczko@gmai.com>
2026-05-28 17:35:33 +0200

    feat: update language, add favicon and simplify page components

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,19 +1,6 @@
 import React from 'react'
 import { About } from '../components/sections/about'
-import { Container, Section } from '../components/layout/container'
 
 export function AboutPage() {
-  return (
-    <div>
-      <Section padding="pt-32 pb-20" className="bg-deep-900">
-        <Container>
-          <div className="text-center mb-16">
-            <h1 className="text-4xl md:text-5xl font-bold text-white mb-4">O Mnie</h1>
-            <p className="text-deep-400 text-lg">Inżynieria, badania i rozwój</p>
-          </div>
-          <About />
-        </Container>
-      </Section>
-    </div>
-  )
+  return <About />
 }
